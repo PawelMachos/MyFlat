6b8407f10871df7afce984a7c0d4d320e7f6335f
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-02-15 14:34:51 +0100

    User entity version beta

---
+++ src/main/java/pl/com/app/myflat/model/entities/User.java
@@ -0,0 +1,29 @@
+package pl.com.app.myflat.model.entities;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column
+    String username;
+    @Column
+    String password;
+    @Column
+    String firstName;
+    @Column
+    String lastName;
+    @Column
+    String email;
+    @Column
+    String flatNumber;
+    @Column
+    String role;
+    boolean active;
+
+}
