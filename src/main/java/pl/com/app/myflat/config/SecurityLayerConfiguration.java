ae64c0390cbf1bd2ad4cda0767a5928b55ff0467
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-02-15 14:12:54 +0100

    Security Configuration

---
+++ src/main/java/pl/com/app/myflat/config/SecurityLayerConfiguration.java
@@ -0,0 +1,56 @@
+package pl.com.app.myflat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Slf4j
+public class SecurityLayerConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityLayerConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT username, password, enabled FROM example_users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, role_name FROM example_users_roles WHERE username = ?");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register", "register/**").permitAll()
+                .antMatchers("/login", "/logout").authenticated()
+                .antMatchers("/user", "/user/**").hasRole("USER")
+                .antMatchers("/manager", "/manager/**").hasRole("MANAGER")
+                .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
