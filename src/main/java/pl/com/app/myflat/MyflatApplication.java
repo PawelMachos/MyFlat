2caa1474b35c7354a3c55c6f6b3ad27187920aa9
Marta Przybyło <marta.m.przybylo@gmail.com>
2020-02-15 12:00:42 +0100

    Initial settings

---
+++ src/main/java/pl/com/app/myflat/MyflatApplication.java
@@ -0,0 +1,13 @@
+package pl.com.app.myflat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyflatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyflatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/app/myflat/MyflatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.app.myflat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyflatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
