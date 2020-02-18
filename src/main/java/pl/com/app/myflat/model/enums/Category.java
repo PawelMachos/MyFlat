7035e88a8895a2393c2c5d07457c8dad1d499bf4
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-02-18 19:35:34 +0100

    category of bill

---
+++ src/main/java/pl/com/app/myflat/model/enums/Category.java
@@ -0,0 +1,12 @@
+package pl.com.app.myflat.model.enums;
+
+public enum Category {
+    EARMARKED_FUND,
+    REPAIR_FUND,
+    CO_AND_CW_FIXED_FEE,
+    MANAGEMENT_COSTS,
+    CENTRAL_HEATING,
+    WASTE_DISPOSAL,
+    WATER_HEATING,
+    COLD_WATER_AND_SEWAGE;
+}
