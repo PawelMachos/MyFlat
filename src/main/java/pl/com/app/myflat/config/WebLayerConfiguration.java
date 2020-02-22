d940b8e9928566c1d027c003831454c39ab1a5e2
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-02-22 11:47:19 +0100

    css upgrade

--- src/main/java/pl/com/app/myflat/config/WebLayerConfiguration.java
+++ src/main/java/pl/com/app/myflat/config/WebLayerConfiguration.java
@@ -1,6 +1,7 @@
 package pl.com.app.myflat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,12 @@ public class WebLayerConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+//        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+        // manualnie zmuszenie Springa do traktowania tej sciezki jako statyczna z css i js
+    }
+
+    @Override// Tomcat domyslnie potrafi rozroznic co jest statyczne(css, js ) a co javowe
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
