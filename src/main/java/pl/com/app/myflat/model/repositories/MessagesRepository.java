8b320e2eeee6baf244d62aae67c9247fde70eef3
Beata <bea.swirko@gmail.com>
2020-02-23 13:30:07 +0100

    new commit

--- src/main/java/pl/com/app/myflat/model/repositories/MessagesRepository.java
+++ src/main/java/pl/com/app/myflat/model/repositories/MessagesRepository.java
@@ -1,9 +1,9 @@
 //package pl.com.app.myflat.model.repositories;
 //
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import pl.com.app.myflat.model.entities.Message;
+//        import org.springframework.data.jpa.repository.JpaRepository;
+//        import pl.com.app.myflat.model.entities.Message;
 //
-//import java.util.List;
+//        import java.util.List;
 //
 //public interface MessagesRepository extends JpaRepository<Message, Long> {
 //
